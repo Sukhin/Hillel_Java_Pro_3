a679e145727d6688c6e945aa2a4ead6b81a08786
Vitaly_Sukhin <VVS11101992@gmail.com>
2026-05-04 02:34:37 +0300

    Functional for converting degrees Fahrenheit to degrees  Celsius

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,9 +2,21 @@ package app;
 
 public class Main {
 
+    private static final double CONV_K1 = 0.56;
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to the program for converting units of temperature!");
+        System.out.print("\n");
+        double FahrenheitInput = 70.2;
+        double CelsiusOutput = convFtoC(FahrenheitInput);
+        System.out.printf("Value of temperature in %.3f degrees Fahrenheit, converting to %.3f degrees Celsius.", FahrenheitInput, CelsiusOutput);
+
+    }
+
+    private static double convFtoC(double fahrenheit) {
+
+        return CONV_K1 * (fahrenheit - 32);
 
     }
 
