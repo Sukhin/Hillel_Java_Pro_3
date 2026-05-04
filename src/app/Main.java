e5b2edadc291804b010ec0268c3e93985d53fe56
Vitaly_Sukhin <VVS11101992@gmail.com>
2026-05-05 00:40:40 +0300

    Corrected the code of the program

--- src/app/Main.java
+++ src/app/Main.java
@@ -9,22 +9,21 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Welcome to the program for converting units of temperature!");
-        System.out.print("\n");
         double fahrenheitInput = 70.2;
         double celsiusInput = 40.5;
-        double celsiusOutput = convFtoC(fahrenheitInput);
-        double fahrenheitOutput = convCtoF(celsiusInput);
+        double celsiusOutput = convFahtoCel(fahrenheitInput);
+        double fahrenheitOutput = convCeltoFah(celsiusInput);
         System.out.printf("Value of temperature in %.3f degrees Fahrenheit, converting to %.3f degrees Celsius. %n", fahrenheitInput, celsiusOutput);
         System.out.printf("Value of temperature in %.3f degrees Celsius, converting to %.3f degrees Fahrenheit. %n", celsiusInput, fahrenheitOutput);
 
     }
 
-    private static double convFtoC(double fahrenheit) {
+     private static double convFahtoCel(double fahrenheit) {
 
         return FAHRENHEIT_TO_CELSIUS_MULTIPLIER * (fahrenheit - CONVERSION_OFFSET);
     }
 
-    private static double convCtoF(double celsius) {
+     private static double convCeltoFah(double celsius) {
 
         return CELSIUS_TO_FAHRENHEIT_MULTIPLIER * celsius + CONVERSION_OFFSET;
     }
