0eaaa4d394ec8aac1ead2d8be662f7de5e2519df
Vitaly_Sukhin <VVS11101992@gmail.com>
2026-05-04 23:43:40 +0300

    Corrected the code style and design

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,34 +2,30 @@ package app;
 
 public class Main {
 
-    private static final double CONV_K1 = 0.56;
-    private static final double CONV_K2 = 1.8;
+    private static final double FAHRENHEIT_TO_CELSIUS_MULTIPLIER = 0.56;
+    private static final double CELSIUS_TO_FAHRENHEIT_MULTIPLIER = 1.8;
+    private static final double CONVERSION_OFFSET = 32;
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to the program for converting units of temperature!");
         System.out.print("\n");
-        double FahrenheitInput = 70.2;
-        double CelsiusInput = 40.5;
-        double CelsiusOutput = convFtoC(FahrenheitInput);
-        double FahrenheitOutput = convCtoF(CelsiusInput);
-
-        System.out.printf("Value of temperature in %.3f degrees Fahrenheit, converting to %.3f degrees Celsius.", FahrenheitInput, CelsiusOutput);
-        System.out.print("\n");
-        System.out.printf("Value of temperature in %.3f degrees Celsius, converting to %.3f degrees Fahrenheit.", CelsiusOutput, FahrenheitOutput);
+        double fahrenheitInput = 70.2;
+        double celsiusInput = 40.5;
+        double celsiusOutput = convFtoC(fahrenheitInput);
+        double fahrenheitOutput = convCtoF(celsiusInput);
+        System.out.printf("Value of temperature in %.3f degrees Fahrenheit, converting to %.3f degrees Celsius. %n", fahrenheitInput, celsiusOutput);
+        System.out.printf("Value of temperature in %.3f degrees Celsius, converting to %.3f degrees Fahrenheit. %n", celsiusInput, fahrenheitOutput);
 
     }
 
     private static double convFtoC(double fahrenheit) {
 
-        return CONV_K1 * (fahrenheit - 32);
-
+        return FAHRENHEIT_TO_CELSIUS_MULTIPLIER * (fahrenheit - CONVERSION_OFFSET);
     }
 
     private static double convCtoF(double celsius) {
 
-        return CONV_K2 * celsius + 32;
-
+        return CELSIUS_TO_FAHRENHEIT_MULTIPLIER * celsius + CONVERSION_OFFSET;
     }
-
 }
